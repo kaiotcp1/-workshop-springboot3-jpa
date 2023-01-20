9360d427baec5544d6a113c285eefd50f0dc8639
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-01-20 15:28:01 -0300

    Exception handling - findById

--- src/main/java/kaio/developer/project/services/UserService.java
+++ src/main/java/kaio/developer/project/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import kaio.developer.project.entities.User;
 import kaio.developer.project.repositories.UserRepository;
+import kaio.developer.project.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
